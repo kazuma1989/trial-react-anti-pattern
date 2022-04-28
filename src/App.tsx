182df4e503f3600ab2698531b973183b86eaa974
kazuma1989 <kazuma.ebina@quipper.com>
2022-04-28 14:37:15 +0900

    video要素の再生中・停止中がステートと乖離するサンプルを追加
    video要素を右クリックして「すべてのコントロールを表示」したり、「ピクチャー イン ピクチャー」したりすると、乖離が起こせる。

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { css } from "@emotion/css"
+import { useEffect, useRef, useState } from "react"
 
 const COLOR = "blue"
 
 export function App() {
+  const [play, setPlay] = useState(false)
+
   return (
     <div
       className={css`
@@ -10,6 +13,38 @@ export function App() {
       `}
     >
       <h1>Hello</h1>
+
+      <Video play={play} />
+
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setPlay((v) => !v)
+          }}
+        >
+          {play ? "再生中" : "停止中"}
+        </button>
+      </div>
     </div>
+  )
+}
+
+function Video({ play }: { play?: boolean }) {
+  const video$ = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    if (play) {
+      video$.current?.play()
+    } else {
+      video$.current?.pause()
+    }
+  }, [play])
+
+  return (
+    <video
+      ref={video$}
+      src="http://iandevlin.github.io/mdn/video-player-with-captions/video/sintel-short.mp4"
+    />
   )
 }
