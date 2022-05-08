48a0f9fe204ed106a742ad09525a66a2f5018b4a
kazuma1989 <kazuma.ebina@quipper.com>
2022-05-08 15:06:25 +0900

    GoodVideoにもOverlayを追加

--- src/GoodVideo.tsx
+++ src/GoodVideo.tsx
@@ -1,4 +1,5 @@
-import { useImperativeHandle, useRef } from "react"
+import { css } from "@emotion/css"
+import { useImperativeHandle, useRef, useState } from "react"
 
 interface Props {
   src?: string
@@ -13,7 +14,10 @@ export interface VideoRef {
 }
 
 export function GoodVideo({ src, methodRef, onPlay, onPause }: Props) {
+  const [play, setPlay] = useState(false)
+
   const video$ = useRef<HTMLVideoElement>(null)
+  const videoPlaying$ = useRef(false)
 
   useImperativeHandle(methodRef, () => ({
     play() {
@@ -25,5 +29,91 @@ export function GoodVideo({ src, methodRef, onPlay, onPause }: Props) {
     },
   }))
 
-  return <video ref={video$} src={src} onPlay={onPlay} onPause={onPause} />
+  return (
+    <VideOverlay
+      play={play}
+      onClick={() => {
+        if (videoPlaying$.current) {
+          video$.current?.pause()
+        } else {
+          video$.current?.play()
+        }
+
+        setPlay((v) => !v)
+      }}
+    >
+      <video
+        ref={video$}
+        src={src}
+        controls={false}
+        onPlay={() => {
+          videoPlaying$.current = true
+          setPlay(true)
+
+          onPlay?.()
+        }}
+        onPause={() => {
+          videoPlaying$.current = false
+          setPlay(false)
+
+          onPause?.()
+        }}
+        className={css`
+          width: 100%;
+          display: block;
+        `}
+      />
+    </VideOverlay>
+  )
+}
+
+interface VideoOverlayProps {
+  play?: boolean
+  onClick?(): void
+  children?: React.ReactNode
+}
+
+function VideOverlay({ play, onClick, children }: VideoOverlayProps) {
+  return (
+    <div
+      className={css`
+        position: relative;
+      `}
+    >
+      {children}
+
+      <button
+        type="button"
+        onClick={onClick}
+        className={css`
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+
+          background-color: rgba(0, 0, 0, 0.3);
+          color: white;
+          border: none;
+          padding: 0;
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 96px;
+
+          transition: opacity 300ms;
+          :not(:hover > &):not(&:focus-visible) {
+            opacity: 0;
+          }
+        `}
+      >
+        {play ? (
+          <i className="bi bi-pause-fill"></i>
+        ) : (
+          <i className="bi bi-play-fill"></i>
+        )}
+      </button>
+    </div>
+  )
 }
--- src/Page2.tsx
+++ src/Page2.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Container, Stack } from "react-bootstrap"
 import { BadVideo } from "./BadVideo"
+import { GoodVideo } from "./GoodVideo"
 
 export function Page2() {
   const [badPlay, setBadPlay] = useState(false)
@@ -20,6 +21,12 @@ export function Page2() {
               setBadPlay(!badPlay)
             }}
           />
+        </div>
+
+        <div>
+          <p>Good video</p>
+
+          <GoodVideo src="http://iandevlin.github.io/mdn/video-player-with-captions/video/sintel-short.mp4" />
         </div>
       </Stack>
     </Container>
