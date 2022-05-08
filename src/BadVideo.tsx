2a423e884f03083eb77b7107955dd5b7e5f0a6a2
kazuma1989 <kazuma.ebina@quipper.com>
2022-05-08 14:56:32 +0900

    BadVideoにVideOverlayを足してみた
    :focus-visibleなるセレクタが便利。

--- src/BadVideo.tsx
+++ src/BadVideo.tsx
@@ -1,11 +1,13 @@
+import { css } from "@emotion/css"
 import { useEffect, useRef } from "react"
 
 interface Props {
   play?: boolean
   src?: string
+  onClick?(): void
 }
 
-export function BadVideo({ play, src }: Props) {
+export function BadVideo({ play, src, onClick }: Props) {
   const video$ = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
@@ -16,5 +18,68 @@ export function BadVideo({ play, src }: Props) {
     }
   }, [play])
 
-  return <video ref={video$} src={src} />
+  return (
+    <VideOverlay play={play} onClick={onClick}>
+      <video
+        ref={video$}
+        src={src}
+        controls={false}
+        className={css`
+          width: 100%;
+          display: block;
+        `}
+      />
+    </VideOverlay>
+  )
+}
+
+interface VideoOverlayProps {
+  play?: boolean
+  onClick?(): void
+  children?: React.ReactNode
+}
+
+function VideOverlay({ play, onClick, children }: VideoOverlayProps) {
+  return (
+    <div
+      className={css`
+        position: relative;
+      `}
+    >
+      {children}
+
+      <button
+        type="button"
+        onClick={onClick}
+        className={css`
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+
+          background-color: rgba(0, 0, 0, 0.3);
+          color: white;
+          border: none;
+          padding: 0;
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 96px;
+
+          transition: opacity 300ms;
+          :not(:hover > &):not(&:focus-visible) {
+            opacity: 0;
+          }
+        `}
+      >
+        {play ? (
+          <i className="bi bi-pause-fill"></i>
+        ) : (
+          <i className="bi bi-play-fill"></i>
+        )}
+      </button>
+    </div>
+  )
 }
--- src/Page2.tsx
+++ src/Page2.tsx
@@ -1,9 +1,27 @@
-import { Container } from "react-bootstrap"
+import { useState } from "react"
+import { Container, Stack } from "react-bootstrap"
+import { BadVideo } from "./BadVideo"
 
 export function Page2() {
+  const [badPlay, setBadPlay] = useState(false)
+
   return (
     <Container fluid="xxl" className="py-3">
-      PAGE 2
+      <Stack gap={3}>
+        <p>PAGE 2</p>
+
+        <div>
+          <p>Bad video</p>
+
+          <BadVideo
+            play={badPlay}
+            src="http://iandevlin.github.io/mdn/video-player-with-captions/video/sintel-short.mp4"
+            onClick={() => {
+              setBadPlay(!badPlay)
+            }}
+          />
+        </div>
+      </Stack>
     </Container>
   )
 }
