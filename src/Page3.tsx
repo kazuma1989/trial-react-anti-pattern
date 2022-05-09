eee4efe9bb0f71dcd570b3d386f86dfccb9baac4
kazuma1989 <kazuma.ebina@quipper.com>
2022-05-09 23:40:39 +0900

    動画のサムネイルをjpegとして生成

--- src/Page3.tsx
+++ src/Page3.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react"
 import { Container, Stack, Table } from "react-bootstrap"
 
 export function Page3() {
+  const [thumbnail, setImg] = useState("")
+
   return (
     <Container fluid="xxl" className="py-3">
       <Stack gap={3}>
+        <div>
+          <input
+            type="file"
+            accept="video/*"
+            onChange={async (e) => {
+              const thumbnail = await generateVideoThumbnail(
+                e.target.files![0]!
+              )
+
+              setImg(thumbnail)
+            }}
+          />
+
+          {thumbnail && <img src={thumbnail} alt="動画のサムネイル" />}
+        </div>
+
         <Table hover striped>
           <thead>
             <tr>
@@ -39,4 +58,27 @@ export function Page3() {
       </Stack>
     </Container>
   )
+}
+
+function generateVideoThumbnail(file: File): Promise<string> {
+  return new Promise((resolve) => {
+    const video = document.createElement("video")
+    video.autoplay = true
+    video.muted = true
+    video.src = URL.createObjectURL(file)
+
+    const canvas = document.createElement("canvas")
+    video.onloadeddata = () => {
+      const ctx = canvas.getContext("2d")!
+
+      canvas.height = Math.min(500, video.videoHeight)
+      canvas.width = (video.videoWidth * canvas.height) / video.videoHeight
+
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+      video.pause()
+
+      resolve(canvas.toDataURL("image/jpeg"))
+    }
+  })
 }
